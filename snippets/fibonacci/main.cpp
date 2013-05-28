9e20117424974ac0d81559af4479677ba8cd7c87
Alex Gurrola <alexgurrola@debian-laptop>
2013-05-27 17:35:14 -0700

    Completed Fibonacci Calculator

--- snippets/fibonacci/main.cpp
+++ snippets/fibonacci/main.cpp
@@ -7,21 +7,30 @@ using namespace std;
 int main(int argc, char** argv) {
 	int n, c, first = 0, second = 1, next;
 	
-	cout << "Enter the number of terms of Fibonacci series you want" << endl;
+	cout << "Enter the number of Fibonacci terms you wish to calculate: ";
+	
+	input:
 	cin >> n;
 	
-	cout << "First " << n << " terms of Fibonacci series are :- " << endl;
+	if ( n > 47 ) {
+		cout << "Enter a number less than 48 for accurate results: ";
+		goto input;
+	}
+	
+	cout << "The first " << n << " terms of the Fibonacci series are: " << endl;
 	
 	for ( c = 0 ; c < n ; c++ ) {
-		if ( c <= 1 )
-			next = c;
+		if ( c <= 1 ) { next = c; }
 		else {
 			next = first + second;
 			first = second;
 			second = next;
 		}
-		cout << next << endl;
+		if (next > 0) { cout << ", "; }
+		cout << next;
 	}
+	
+	cout << endl;
 	
 	return 0;
 }
